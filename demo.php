c2a38aeb7f7daba65081094a989edcc7ca900eea
VisikLost <106622121+VisikLost@users.noreply.github.com>
2024-01-29 10:29:20 +0100

    Add files via upload

--- demo.php
+++ demo.php
@@ -23,16 +23,49 @@
             width: 40%;
             gap: 40px;
         }
+
+        .NumberText {
+            padding: 10px;
+            text-align: center;
+            width: 50%;
+            margin: 0 auto;
+        }
+
+        .dropdownlist {
+            width: 50%;
+            margin: 0 auto;
+            padding: 5px;
+            text-align: center;
+            -webkit-appearance: none;
+        }
+
+        .count {
+            padding: 5px;
+            width: 30%;
+            margin: 0 auto;
+        }
     </style>
 
 </head>
 <body>
 
     <form action="">
-        <input type="text">
-        <input type="text">
-        <h1>Vysledek</h1>
+        <input type="text" class="NumberText" placeholder="ČÍSLO1">
+
+        <select name="operations" id="" class="dropdownlist" >
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value=":">:</option>
+        </select>
+
+        <input type="text" class="NumberText" placeholder="ČÍSLO2">
+
+        <input type="submit" value="Spočítač" class="count">
+        <h1 id="result">/-_-/</h1>
     </form>
+
+
 
     <?php
 
