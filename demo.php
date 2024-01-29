beea86e7902876db395da36034afb73b15417e7d
VisikLost <106622121+VisikLost@users.noreply.github.com>
2024-01-29 08:31:24 +0100

    Add files via upload

---
+++ demo.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Kalkulačka</title>
+
+    <style>
+        * {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+
+        form {
+            text-align: center;
+            margin: 0 auto;
+            margin-top: 100px;
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            width: 40%;
+            gap: 40px;
+        }
+    </style>
+
+</head>
+<body>
+
+    <form action="">
+        <input type="text">
+        <input type="text">
+        <h1>Vysledek</h1>
+    </form>
+
+    <?php
+
+    ?>
+</body>
+</html>
